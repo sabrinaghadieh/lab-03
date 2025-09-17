6d4924462c68c56c67f9a81b7db194f10c48a6b1
Sabrina <sabrinaghadieh@gmail.com>
2025-09-17 12:02:24 -0600

    completed lab 3

--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.listycitylab3;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +37,34 @@ public class MainActivity extends AppCompatActivity {
         cityList = findViewById(R.id.city_list);
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedCity = dataList.get(position);
+            View dialogView = getLayoutInflater().inflate(R.layout.dialog_edit_city, null);
+            EditText cityInput = dialogView.findViewById(R.id.editCityName);
+            EditText provinceInput = dialogView.findViewById(R.id.editProvinceName);
+
+            // make fields already filled with current city and province
+            String[] parts = selectedCity.split(",",2);
+            String cityPart = parts.length > 0 ? parts[0].trim() : "";
+            String provincePart = parts.length > 1 ? parts[1].trim() : "";
+
+            cityInput.setText(cityPart);
+            cityInput.setSelection(cityPart.length());
+            provinceInput.setText(provincePart);
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Edit city")
+                    .setView(dialogView)
+                    .setPositiveButton("Save", (dialog, which) -> {
+                        String newCity = cityInput.getText().toString().trim();
+                        String newProvince = provinceInput.getText().toString().trim();
+                        dataList.set(position, newCity + ", " + newProvince);
+                        cityAdapter.notifyDataSetChanged();
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        });
+
     }
 }
